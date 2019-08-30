fixed a major problem in the neural network, now it acutally learns, using a simpler architectur for now. added normalization for frames and some other minor tweaks

sonic/sonic_multi_input.py
@@ -60,7 +60,7 @@ def main():
     pprint (data)
 
     # Parameters
-    timesteps = 20000#4500
+    timesteps = 6000#4500
     memory = deque(maxlen=30000)
     epsilon = 0.3                                #probability of doing a random move
     epsilon_decay = 0.99  #will be multiplied with epsilon for decaying it
@@ -69,9 +69,8 @@ def main():
     rand_decay = 1e-3                                #reduce the randomness by decay/loops
     gamma = 0.99                               #discount for future reward
     mb_size = 256                               #learning minibatch size
-    loops = 10#45                               #loop through the different game levels
-    sub_loops = 10#100
-    hold_action = 1                            #nb frames during which we hold (4 for normal, 1 for contest)
+    loops = 35#45                               #loop through the different game levels
+    sub_loops = 35#100
     learning_rate = 5e-5
     max_reward = 0
     min_reward = 10000
@@ -79,18 +78,16 @@ def main():
     #action_threshold = 1
     target_step_interval = 10
     reward_clip = 200
-    resize_to = [128,128]
+    image_size = (128,128,frames_stack)
     save_factor= 500 # when to save the model according to loops_count
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True # pylint: disable=E1101
     converged=False # flag to check convergence (diff between Q and Q_target is small enough)
     with tf.Session(config=config) as sess:
-        model=effnet.Effnet(input_shape=(84,84,frames_stack),nb_classes=ACTION_SIZE, info=11)
+        model=effnet.Effnet(input_shape=(128,128,frames_stack),nb_classes=ACTION_SIZE, info=11)
         if os.path.isfile("sonic_model.h5"):
             model.load_weights("sonic_model.h5")
-
         model.compile(loss="mse", optimizer=optimizers.Adam(lr=learning_rate), metrics=["accuracy"])
-
         tensorboard = TensorBoard(log_dir="logs/sonic_modmemdecayrdq18_reshape_64x512mb256_resc_target_interval_{}_memory_30000_lr_{}_decay_{}.{}".format(target_step_interval,learning_rate, rand_decay, time.time()))
         tensorboard.set_model(model)
         train_names = ["Loss", "Accuracy"]
@@ -108,6 +105,8 @@ def main():
     for training_loop in range(loops):
         if converged:
             # model converged
+            model.save_weights("sonic_model.h5")
+            model.save_weights("sonic_target_model.h5")
             print("Model converged, stopping training")
             break
         with tf.Session(config=config) as sess:
@@ -118,7 +117,6 @@ def main():
             target_model.load_weights("sonic_target_model.h5")
             target_model.trainable = False
             target_model.compile(loss="mse", optimizer=optimizers.Adam(lr=learning_rate), metrics=["accuracy"])
-
             for sub_training_loop in range(sub_loops):
                 loop_start_time = time.time()
                 game = np.random.choice(games,1)[0]
@@ -128,7 +126,7 @@ def main():
                 print("Playing",game,"-",state)
                 #env = AllowBacktracking(retro.make(game, state,scenario="scenario.json", record="logs/"))
                 env = retro.make(game, state,scenario="scenario.json", record="logs/")
-                env = wrappers.WarpFrame(env, 84, 84, grayscale=True)
+                env = wrappers.WarpFrame(env, 128, 128, grayscale=True)
                 env = wrappers.FrameStack(env,frames_stack)
                 env = wrappers.SonicDiscretizer(env) # Discretize the environment for q learning
                 env = wrappers.RewardWrapper(env) # custom reward calculation
@@ -141,17 +139,15 @@ def main():
                 #in this loop sonic only plays according to epsilon greedy and saves its experience
                 for t in range(timesteps):
                     #env.render() #display training
-                    if np.random.rand() > epsilon and t>0:
-                        Q = model.predict([obs[np.newaxis,:],info[np.newaxis,:]])[0]          # Q-values predictions
+                    if np.random.rand() > epsilon and sub_training_loop>0:
+                        Q = model.predict([np.array(obs)[np.newaxis,:],info[np.newaxis,:]])[0]          # Q-values predictions
                         action = np.argmax(Q)
                     else:
                         #pick a random action
                         action = env.action_space.sample()
                     next_obs, reward, done, info = env.step(action)     # result of action
-                    #reward = reward_calc(last_info,info,done)
                     info_dic=info
                     info = np.array(list(info_dic.values()))
-                    next_obs = np.array(next_obs) #converts from Lazy format to normal numpy array see wrappers_atari.py
                     reward_ = min(reward,reward_clip)
 
                     total_raw_reward += reward
@@ -179,7 +175,7 @@ def main():
                     minibatch = random.sample(memory, mb_size)
 
                     info_inputs = np.zeros((mb_size,11))
-                    inputs_shape = (mb_size,) + obs.shape
+                    inputs_shape = (mb_size,) + image_size
                     inputs = np.zeros(inputs_shape)
                     targets = np.zeros((mb_size, env.action_space.n))
 
@@ -198,24 +194,15 @@ def main():
                         target_model.trainable = False
                         target_model.compile(loss="mse", optimizer=optimizers.Adam(lr=learning_rate), metrics=["accuracy"])
                     diff=0
-                    for i in range(0, mb_size):
-                        obs = minibatch[i][0]
-                        next_obs = minibatch[i][1]
-                        action = minibatch[i][2]
-                        reward = minibatch[i][3]
-                        done = minibatch[i][4]
-                        info = minibatch[i][5]
-
-                        #reward clipping
+                    for i,(obs,next_obs,action,reward,done,info) in enumerate(minibatch):
                         reward = min(reward,reward_clip)
-
-                        #Bellman double Q
-                        inputs[i] = obs[np.newaxis,:]
+                        obs=np.array(obs)
+                        next_obs=np.array(next_obs)
+                        inputs[i] = obs
                         info_inputs[i] = info
                         Q = model.predict([obs[np.newaxis,:],info[np.newaxis,:]])[0]          # Q-values predictions
                         Q_next = model.predict([next_obs[np.newaxis,:],info[np.newaxis,:]])[0]
                         Q_target = target_model.predict([next_obs[np.newaxis,:],info[np.newaxis,:]])[0]
-
                         targets[i] = copy.copy(Q)
                         diff+=sum(Q-Q_target)
                         if done:
@@ -231,7 +218,7 @@ def main():
                     print("Model minibatch training lasted:",
                           str(timedelta(seconds=time.time()-minibatch_train_start_time)),"dd:hh:mm:ss")
                     print("Learning Finished",sub_training_loop+1,"x",training_loop+1,"out of",sub_loops,"x",loops)
-                    if diff/mb_size < 0.000000000001 and training_loop > 5:
+                    if diff/mb_size < 0.000000000000001 and training_loop > 5:
                         # if there is not much difference in a batch after some
                         # training assume convergance
                         converged = True

sonic/wrappers.py
@@ -1,4 +1,9 @@
 import gym
+import numpy as np
+from collections import deque
+from gym import spaces
+import cv2
+cv2.ocl.setUseOpenCL(False)
 
 class TimeLimit(gym.Wrapper):
     def __init__(self, env, max_episode_steps=None):
@@ -20,10 +25,176 @@ class TimeLimit(gym.Wrapper):
 
 class ClipActionsWrapper(gym.Wrapper):
     def step(self, action):
-        import numpy as np
         action = np.nan_to_num(action)
         action = np.clip(action, self.action_space.low, self.action_space.high)
         return self.env.step(action)
 
     def reset(self, **kwargs):
         return self.env.reset(**kwargs)
+class RewardWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.last_info=dict([])
+    def reset(self, **kwargs):
+        self.last_info = dict([])
+        return self.env.reset(**kwargs)
+    def step(self, action):
+        obs, rew, done, info = self.env.step(action)
+        rew=0 # ignore default rew and calculate one based on info and last_info
+        if len(self.last_info) > 0:
+            # no reward estimation for first step
+            if info["level_end_bonus"] > 0:
+                # happens only if level is won
+                rew+=5000
+            if info["x"] > self.last_info["x"]:
+                rew+=10
+            if info["x"] == self.last_info["x"]:
+                # motivate movement on the x axis
+                rew-=1
+            if info["lives"] < self.last_info["lives"]:
+                rew-=100
+            if info["rings"] > self.last_info["rings"]:
+                rew+=20
+            if self.last_info["rings"] > 0 and info["rings"] == 0:
+                # if we were attacked and lost all rings
+                rew-=30
+        self.last_info = info
+        return obs, rew, done, info
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env, width=84, height=84, grayscale=True, dict_space_key=None):
+        """
+        Warp frames to 84x84 as done in the Nature paper and later work.
+        If the environment uses dictionary observations, `dict_space_key` can be specified which indicates which
+        observation should be warped.
+        """
+        super().__init__(env)
+        self._width = width
+        self._height = height
+        self._grayscale = grayscale
+        self._key = dict_space_key
+        if self._grayscale:
+            num_colors = 1
+        else:
+            num_colors = 3
+
+        new_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(self._height, self._width, num_colors),
+            dtype=np.uint8,
+        )
+        if self._key is None:
+            original_space = self.observation_space
+            self.observation_space = new_space
+        else:
+            original_space = self.observation_space.spaces[self._key]
+            self.observation_space.spaces[self._key] = new_space
+        assert original_space.dtype == np.uint8 and len(original_space.shape) == 3
+
+    def observation(self, obs):
+        if self._key is None:
+            frame = obs
+        else:
+            frame = obs[self._key]
+        frame = cv2.normalize(frame, frame, 0, 255, cv2.NORM_MINMAX)
+        if self._grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(
+            frame, (self._width, self._height), interpolation=cv2.INTER_AREA
+        )
+        if self._grayscale:
+            frame = np.expand_dims(frame, -1)
+
+        if self._key is None:
+            obs = frame
+        else:
+            obs = obs.copy()
+            obs[self._key] = frame
+        return obs
+
+class LazyFrames(object):
+    def __init__(self, frames):
+        """This object ensures that common frames between the observations are only stored once.
+        It exists purely to optimize memory usage which can be huge for DQN's 1M frames replay
+        buffers.
+        This object should only be converted to numpy array before being passed to the model.
+        You'd not believe how complex the previous solution was."""
+        self._frames = frames
+        self._out = None
+
+    def _force(self):
+        if self._out is None:
+            self._out = np.concatenate(self._frames, axis=-1)
+            self._frames = None
+        return self._out
+
+    def __array__(self, dtype=None):
+        out = self._force()
+        if dtype is not None:
+            out = out.astype(dtype)
+        return out
+
+    def __len__(self):
+        return len(self._force())
+
+    def __getitem__(self, i):
+        return self._force()[i]
+
+    def count(self):
+        frames = self._force()
+        return frames.shape[frames.ndim - 1]
+
+    def frame(self, i):
+        return self._force()[..., i]
+
+class SonicDiscretizer(gym.ActionWrapper):
+    """
+    Wrap a gym-retro environment and make it use discrete
+    actions for the Sonic game.
+    """
+    def __init__(self, env):
+        super(SonicDiscretizer, self).__init__(env)
+        buttons = ["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
+        actions = [['LEFT'], ['RIGHT'], ['LEFT', 'DOWN'], ['RIGHT', 'DOWN'], ['DOWN'],
+                   ['DOWN', 'B'], ['B'], ['UP']] #adding UP
+        self._actions = []
+        for action in actions:
+            arr = np.array([False] * 12)
+            for button in action:
+                arr[buttons.index(button)] = True
+            self._actions.append(arr)
+        self.action_space = gym.spaces.Discrete(len(self._actions))
+
+    def action(self, a): # pylint: disable=W0221
+        return self._actions[a].copy()
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        """Stack k last frames.
+        Returns lazy array, which is much more memory efficient.
+        See Also
+        --------
+        baselines.common.atari_wrappers.LazyFrames
+        """
+        gym.Wrapper.__init__(self, env)
+        self.k = k
+        self.frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[:-1] + (shp[-1] * k,)), dtype=env.observation_space.dtype)
+
+    def reset(self):
+        ob = self.env.reset()
+        for _ in range(self.k):
+            self.frames.append(ob)
+        return self._get_ob()
+
+    def step(self, action):
+        ob, reward, done, info = self.env.step(action)
+        self.frames.append(ob)
+        return self._get_ob(), reward, done, info
+
+    def _get_ob(self):
+        assert len(self.frames) == self.k
+        return LazyFrames(list(self.frames))
+