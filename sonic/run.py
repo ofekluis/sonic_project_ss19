409d178e01b4bad4a71642d77c242705ef3a5ecd
Mauritius Klein <mauriklein93@yahoo.de>
2019-09-14 11:00:10 +0200

    changed evaluation parameter set and made some minor changes

--- sonic/run.py
+++ sonic/run.py
@@ -3,11 +3,16 @@ import sonic_multi_input
 #run multiple trainings
 def main():
 	#evaluation Training sets -> currently 72 trainings
-	epsilonList=[0.1, 0.3, 0.5, 0.7]
+	epsilonList=[0.1, 0.3, 0.5]
 	experimentList=[2000]
 	timeStepsList=[5000,10000,15000]
-	mb_sizeList=[32,64,128]
-	frameStacksList=[4,6]
+	mb_sizeList=[32,128]
+	frameStacksList=[4]
+	#epsilonList=[0.1, 0.3, 0.5, 0.7]
+	#experimentList=[2000]
+	#timeStepsList=[5000,10000,15000]
+	#mb_sizeList=[32,64,128]
+	#frameStacksList=[4,6]
 
 	for e in epsilonList:
 		for ex in experimentList:
